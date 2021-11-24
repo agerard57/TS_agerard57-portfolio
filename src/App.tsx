d325fe9d727a9fbba0201048593ca7f51386051d
agerard57 <agerard57@protonmail.com>
2021-11-24 09:04:05 +0100

    Add TODO

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import "./App.css";
 import "./i18n";
 import { RouteManager } from "./routeManager";
 
+//TODO Proper Suspense
+
 export const App: FC = () => (
   <Suspense fallback={<div>deez</div>}>
     <RouteManager />
