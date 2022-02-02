95e120575257d6e6b4d1e7f8042f320da05c4ee8
agerard57 <agerard57@protonmail.com>
2022-02-02 11:53:01 +0100

    Add SVG's to the asset folder

---
+++ src/skills/assets/index.ts
@@ -0,0 +1,17 @@
+import afterEffectsLogo from "./after-effects-logo.svg";
+import blenderLogo from "./blender-logo.svg";
+import flsLogo from "./flstudio-logo.svg";
+import gitLogo from "./git-logo.svg";
+import msoLogo from "./office-logo.svg";
+import psdLogo from "./photoshop-logo.svg";
+import vegasProLogo from "./vegas-pro-logo.svg";
+
+export const softwareLogos: Record<string, string> = {
+  ae: afterEffectsLogo,
+  blender: blenderLogo,
+  fls: flsLogo,
+  git: gitLogo,
+  mso: msoLogo,
+  psd: psdLogo,
+  veg: vegasProLogo,
+};
