4945f5c65af6dc8c79eb14b127272b0827f03323
agerard57 <agerard57@protonmail.com>
2022-02-04 15:54:51 +0100

    Change method of translations storage

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -1,21 +1,25 @@
 import i18n from "i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
-import Backend from "i18next-http-backend";
 import { initReactI18next } from "react-i18next";
 
+import { resources } from "./resources";
+
+const lng = ["fr", "en"];
 i18n
   .use(new LanguageDetector())
-  .use(Backend)
   .use(initReactI18next)
   .init({
+    debug: true,
     detection: { order: ["querystring"], lookupQuerystring: "lng" },
     fallbackLng: "en",
-    preload: ["en", "fr"],
     interpolation: { escapeValue: false },
-    returnObjects: true,
+    preload: lng,
     react: {
       bindI18n: "languageChanged",
     },
+    resources,
+    returnObjects: true,
+    supportedLngs: lng,
   });
 
 export { i18n };
---
+++ src/i18n/resources.ts
@@ -0,0 +1,36 @@
+import { i18n as contact } from "../contact";
+import { i18n as core } from "../core";
+import { i18n as landingPage } from "../landingPage";
+import { i18n as projects } from "../landingPage";
+import { i18n as presentation } from "../presentation";
+import { i18n as resume } from "../resume";
+import { i18n as skills } from "../skills";
+
+type Modules = Record<string, { fr: any; en: any }>; // eslint-disable-line @typescript-eslint/no-explicit-any
+
+const normalize = (
+  modules: Modules,
+  formatNamespace: (name: string) => string = (name) => name
+) =>
+  Object.entries(modules).reduce(
+    ({ fr, en }, [namespace, translations]) => ({
+      fr: { ...fr, [formatNamespace(namespace)]: translations.fr },
+      en: { ...en, [formatNamespace(namespace)]: translations.en },
+    }),
+    { fr: {}, en: {} }
+  );
+
+const moduleResources = normalize({
+  contact,
+  core,
+  landingPage,
+  projects,
+  presentation,
+  resume,
+  skills,
+});
+
+export const resources = {
+  fr: { ...moduleResources.fr },
+  en: { ...moduleResources.en },
+};
