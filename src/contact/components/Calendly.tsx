831b4b34b60ae65cc28d37ef3511ae352c25eaa3
agerard57 <agerard57@protonmail.com>
2022-03-03 16:25:17 +0100

    fix: Remove unused component

--- src/contact/components/Calendly.tsx
+++
@@ -1,8 +0,0 @@
-import { FC } from "react";
-import { InlineWidget } from "react-calendly";
-
-export const Calendly: FC = () => (
-  <div className="container">
-    <InlineWidget url="https://calendly.com/agerard57" />
-  </div>
-);
